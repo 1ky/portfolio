b362341078a12b5d7cbcfd7178488d53f4a9eb7a
Kyle Shepherd <kyshepherd@protonmail.com>
2025-03-25 13:09:07 -0400

    Design homepage layout and navbar highlights

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
 	import '../app.css';
+    import { page } from '$app/state';
+
 	let { children } = $props();
 </script>
 
-<nav class="w-full flex justify-center p-8 fixed top-0">
-    <div class="bg-blue-500 text-white p-2 rounded-xl flex gap-4 items-center">
-        <a href="/"><div class="py-1 px-2 bg-blue-300 rounded-lg">Home</div></a>
-        <a href="/projects"><div class="py-1 px-2 rounded-lg">Projects</div></a>
-        <a href="/contact"><div class="py-1 px-2 rounded-lg">Contact</div></a>
+<nav class="w-full flex justify-center p-8 fixed top-0 bg-white/75">
+    <div class="bg-blue-300/75 text-white/75 font-semibold p-2 rounded-xl flex gap-4 items-center">
+        <a href="/"><div class={`py-1 px-2 rounded-lg ${page.route.id === '/' ? 'bg-blue-700 text-white' : ''}`}>Home</div></a>
+        <a href="/projects"><div class={`py-1 px-2 rounded-lg ${page.route.id === '/projects' ? 'bg-blue-700 text-white' : ''}`}>Projects</div></a>
+        <a href="/contact"><div class={`py-1 px-2 rounded-lg ${page.route.id === '/contact' ? 'bg-blue-700 text-white' : ''}`}>Contact</div></a>
     </div>
 </nav>
 
