3e05cac215feca74cdad65945aa48aeb4c0acfe6
Kyle Shepherd <kyshepherd@protonmail.com>
2025-03-24 22:11:33 -0400

    Design navbar and set up page routing

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,4 +3,12 @@
 	let { children } = $props();
 </script>
 
+<nav class="w-full flex justify-center p-8 fixed top-0">
+    <div class="bg-blue-500 text-white p-2 rounded-xl flex gap-4 items-center">
+        <a href="/"><div class="py-1 px-2 bg-blue-300 rounded-lg">Home</div></a>
+        <a href="/projects"><div class="py-1 px-2 rounded-lg">Projects</div></a>
+        <a href="/contact"><div class="py-1 px-2 rounded-lg">Contact</div></a>
+    </div>
+</nav>
+
 {@render children()}
