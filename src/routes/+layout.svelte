ef1078500edf9e2906e3224811fb42e0d90c3cdc
Kyle Shepherd <kyshepherd@protonmail.com>
2025-03-25 20:03:39 -0400

    Design layouts for projects and contact pages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,7 @@
 	let { children } = $props();
 </script>
 
-<nav class="w-full flex justify-center p-8 fixed top-0 bg-white/75">
+<nav class="w-full flex justify-center p-8 fixed top-0 bg-white/75 backdrop-blur-xs">
     <div class="bg-blue-300/75 text-white/75 font-semibold p-2 rounded-xl flex gap-4 items-center">
         <a href="/"><div class={`py-1 px-2 rounded-lg ${page.route.id === '/' ? 'bg-blue-700 text-white' : ''}`}>Home</div></a>
         <a href="/projects"><div class={`py-1 px-2 rounded-lg ${page.route.id === '/projects' ? 'bg-blue-700 text-white' : ''}`}>Projects</div></a>
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,1 +1,7 @@
-<div>Contact</div>
+<div class="flex justify-center w-full h-screen p-4">
+    <div class="grid lg:grid-cols-4 lg:grid-rows-2 grid-flow-row-dense gap-4 w-full lg:max-w-[700px] h-full lg:max-h-[400px]">
+        <div class="lg:col-span-2 lg:row-span-1 bg-blue-300 rounded-3xl flex justify-center items-center">Email</div>
+        <div class="lg:col-span-2 lg:row-span-2 bg-blue-300 rounded-3xl flex justify-center items-center">LinkedIn</div>
+        <div class="lg:col-span-2 lg:row-span-1 bg-blue-300 rounded-3xl flex justify-center items-center">Github</div>
+    </div>   
+</div>
