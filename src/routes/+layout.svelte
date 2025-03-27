960a1a390cb478889e66c65e8cb846bab729a84e
Kyle Shepherd <kyshepherd@protonmail.com>
2025-03-27 00:24:43 -0400

    Implement contact page design

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,34 @@
 <script lang="ts">
 	import '../app.css';
-    import { page } from '$app/state';
+	import { page } from '$app/state';
 
 	let { children } = $props();
 </script>
 
-<nav class="w-full flex justify-center p-8 fixed top-0 bg-black/75 backdrop-blur-xs">
-    <div class="bg-main text-white/50 font-semibold p-2 rounded-xl flex gap-4 items-center">
-        <a href="/"><div class={`py-1 px-2 rounded-lg ${page.route.id === '/' ? 'bg-highlight text-white' : ''}`}>Home</div></a>
-        <a href="/projects"><div class={`py-1 px-2 rounded-lg ${page.route.id === '/projects' ? 'bg-highlight text-white' : ''}`}>Projects</div></a>
-        <a href="/contact"><div class={`py-1 px-2 rounded-lg ${page.route.id === '/contact' ? 'bg-highlight text-white' : ''}`}>Contact</div></a>
-    </div>
+<nav class="fixed top-0 z-30 flex w-full justify-center bg-black/75 p-8 backdrop-blur-xs">
+	<div class="bg-main flex items-center gap-4 rounded-xl p-2 font-semibold text-white/50">
+		<a href="/"
+			><div
+				class={`rounded-lg px-2 py-1 ${page.route.id === '/' ? 'bg-highlight text-white' : ''}`}
+			>
+				Home
+			</div></a
+		>
+		<a href="/projects"
+			><div
+				class={`rounded-lg px-2 py-1 ${page.route.id === '/projects' ? 'bg-highlight text-white' : ''}`}
+			>
+				Projects
+			</div></a
+		>
+		<a href="/contact"
+			><div
+				class={`rounded-lg px-2 py-1 ${page.route.id === '/contact' ? 'bg-highlight text-white' : ''}`}
+			>
+				Contact
+			</div></a
+		>
+	</div>
 </nav>
 
 {@render children()}
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,15 +1,79 @@
-<div class="flex flex-col items-center w-full h-screen p-4">
-    <div class="grid lg:grid-cols-4 lg:grid-rows-8 grid-flow-row-dense gap-4 w-full h-full lg:max-w-[1250px] lg:min-h-[500px]">
-        <div class="lg:col-span-2 lg:row-span-6 bg-main rounded-3xl flex justify-center items-center">Image</div>
-        <div class="lg:col-span-2 lg:row-span-4 bg-main rounded-3xl flex justify-center items-center">Description</div>
-        <div class="lg:col-span-1 lg:row-span-2 bg-main rounded-3xl flex justify-center items-center">Link</div>
-        <div class="lg:col-span-1 lg:row-span-2 bg-main rounded-3xl flex justify-center items-center">Github Link</div>
-    </div>   
+<script>
+	import { SiGithub, SiPolygon } from '@icons-pack/svelte-simple-icons';
+	import animax from '$lib/assets/animax-home.png';
+	import rvgd from '$lib/assets/RVGD.png';
+</script>
 
-    <div class="grid lg:grid-cols-4 lg:grid-rows-8 grid-flow-row-dense gap-4 w-full h-full lg:max-w-[1250px] lg:min-h-[500px]">
-        <div class="lg:col-span-2 lg:row-span-6 bg-main rounded-3xl flex justify-center items-center">Image</div>
-        <div class="lg:col-span-2 lg:row-span-4 bg-main rounded-3xl flex justify-center items-center">Description</div>
-        <div class="lg:col-span-1 lg:row-span-2 bg-main rounded-3xl flex justify-center items-center">Link</div>
-        <div class="lg:col-span-1 lg:row-span-2 bg-main rounded-3xl flex justify-center items-center">Github Link</div>
-    </div> 
+<div class="flex h-screen w-full flex-col items-center p-4">
+	<div
+		class="grid h-full w-full grid-flow-row-dense gap-4 lg:min-h-[500px] lg:max-w-[1250px] lg:grid-cols-4 lg:grid-rows-8"
+	>
+		<div
+			class="bg-main flex items-center justify-center overflow-hidden rounded-3xl lg:col-span-2 lg:row-span-6"
+		>
+			<img src={animax} alt="animax | anime discovery web app" class="object-fit h-full w-full" />
+		</div>
+		<div class="bg-main flex items-center justify-center rounded-3xl lg:col-span-2 lg:row-span-4">
+			<div class="relative h-full w-full p-4">
+				<h1 class="mb-2 text-3xl font-semibold text-white">Animax</h1>
+				<p class="text-white/75">
+					Anime discovery web application. Uses the AniList API for data fetching. For this project,
+					I set the limitation that I would not use any UI component libraries until completion of
+					the first stage of development. Going forward I will incorporate Radix UI to enhance the
+					UX and functionality of the project.
+				</p>
+				<div class="absolute bottom-0 flex justify-start gap-3 p-2">
+					<p class="bg-highlight rounded-full px-2 py-1 text-sm text-white/75">TypeScript</p>
+					<p class="bg-highlight rounded-full px-2 py-1 text-sm text-white/75">React</p>
+					<p class="bg-highlight rounded-full px-2 py-1 text-sm text-white/75">Tailwindcss</p>
+					<p class="bg-highlight rounded-full px-2 py-1 text-sm text-white/75">GraphQL</p>
+				</div>
+			</div>
+		</div>
+		<div class="bg-main flex items-center justify-center rounded-3xl lg:col-span-1 lg:row-span-2">
+			<div class="text-white/75">
+				<SiPolygon title="Link" size={60} />
+			</div>
+		</div>
+		<div class="bg-main flex items-center justify-center rounded-3xl lg:col-span-1 lg:row-span-2">
+			<div class="text-white/75">
+				<SiGithub title="GitHub" size={60} />
+			</div>
+		</div>
+	</div>
+
+	<div
+		class="grid h-full w-full grid-flow-row-dense gap-4 lg:min-h-[500px] lg:max-w-[1250px] lg:grid-cols-4 lg:grid-rows-8"
+	>
+		<div
+			class="bg-main flex items-center justify-center overflow-hidden rounded-3xl lg:col-span-2 lg:row-span-6"
+		>
+			<img src={rvgd} alt="RVGD | videogame discovery web app" class="object-fit h-full w-full" />
+		</div>
+		<div class="bg-main flex items-center justify-center rounded-3xl lg:col-span-2 lg:row-span-4">
+			<div class="relative h-full w-full p-4">
+				<h1 class="mb-2 text-3xl font-semibold text-white">RVGD</h1>
+				<p class="text-white/75">
+					Videogame discovery web application. Uses the RAWG API for data fetching. Chakra UI
+					component library is heavily used for designing the websites UI and functionality.
+				</p>
+				<div class="absolute bottom-0 flex justify-start gap-3 p-2">
+					<p class="bg-highlight rounded-full px-2 py-1 text-sm text-white/75">TypeScript</p>
+					<p class="bg-highlight rounded-full px-2 py-1 text-sm text-white/75">React</p>
+					<p class="bg-highlight rounded-full px-2 py-1 text-sm text-white/75">Chakra UI</p>
+					<p class="bg-highlight rounded-full px-2 py-1 text-sm text-white/75">REST API</p>
+				</div>
+			</div>
+		</div>
+		<div class="bg-main flex items-center justify-center rounded-3xl lg:col-span-1 lg:row-span-2">
+			<div class="text-white/75">
+				<SiPolygon title="Link" size={60} />
+			</div>
+		</div>
+		<div class="bg-main flex items-center justify-center rounded-3xl lg:col-span-1 lg:row-span-2">
+			<div class="text-white/75">
+				<SiGithub title="GitHub" size={60} />
+			</div>
+		</div>
+	</div>
 </div>
